2f1c607700f7f87973a2636775e206acb054ae41
reshma-sk <108371405+reshma-sk@users.noreply.github.com>
2024-06-21 12:03:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accuntId = 23334;
+let accountEmail = 'reshmashaik0910@gmail.com';
+var accountPassword = 12345;
+accountCity = 'Hyderabad'
+let accontState;
+
+// accuntId = 3425;
+accountEmail = 'xyz@gmail.com';
+accountPassword = 3456;
+accountCity = 'dublin';
+console.log(accuntId);
+console.table([accuntId,accountEmail,accountPassword,accountCity,accontState]);
+
+/*
+Prefer not to use var
+bacause of issue in block scope and functional scope
+*/
+
+
